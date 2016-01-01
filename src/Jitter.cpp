22e62751047d98e6bc5560c7a40d0df170f0f83f
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-01-01 17:21:35 -0500

    Added primitives to load and store 64-bits values from references.

--- src/Jitter.cpp
+++ src/Jitter.cpp
@@ -536,6 +536,19 @@ void CJitter::LoadFromRef()
 	m_shadow.Push(tempSym);
 }
 
+void CJitter::Load64FromRef()
+{
+	auto tempSym = MakeSymbol(SYM_TEMPORARY64, m_nextTemporary++);
+
+	STATEMENT statement;
+	statement.op    = OP_LOADFROMREF;
+	statement.src1  = MakeSymbolRef(m_shadow.Pull());
+	statement.dst   = MakeSymbolRef(tempSym);
+	InsertStatement(statement);
+
+	m_shadow.Push(tempSym);
+}
+
 void CJitter::StoreAtRef()
 {
 	STATEMENT statement;
@@ -543,6 +556,11 @@ void CJitter::StoreAtRef()
 	statement.src2	= MakeSymbolRef(m_shadow.Pull());
 	statement.src1	= MakeSymbolRef(m_shadow.Pull());
 	InsertStatement(statement);
+}
+
+void CJitter::Store64AtRef()
+{
+	StoreAtRef();
 }
 
 //64-bits
